8499722fd516524ec31ddaa8c74a209cf38ccc45
Miri Newman <fishernewman@gmail.com>
2023-05-16 12:29:45 -0700

    lights work bettr now

--- LED_Light_for_Capstone/LED_Light_for_Capstone.ino
+++ LED_Light_for_Capstone/LED_Light_for_Capstone.ino
@@ -6,10 +6,12 @@
 #include <math.h>
 
 // IP scheme 10.0.0.x
-// IP scheme 10.0.10.x for ESP
+// IP scheme 10.0.10.x for ESP (rainstick, etc)
 
 IPAddress ip(10, 0, 0, 1); // number these sequentially for each cloud controller, 
 //REMEMEBER TO CHANGE IT!!!!
+
+EthernetClient client;
 
 #define LED_PIN 6
 #define LED_COUNT 90 //change to however many lights we have
@@ -25,66 +27,59 @@ uint32_t orangeRGB = strip.Color(222, 95, 27);
 
 int colorControl = 0;
 
-//enum colorWheel[] = {stormyRGB, whiteRGB, lightBlueRGB, medPinkRGB, orangeRGB};
+bool colorDirectionSwitch = false;
 
 void setup() {
   Serial.begin(9600);
   
   strip.begin();
-  strip.show();
-  //Serial.print(strip.read()); // someone figure something out for this
+  strip.show(); // these reset the LED memory to start afresh
   
 }
 
 void loop() {
-  int randomBrightness = random(100);
-  //int px = 0;
-  //strip.setPixelColor(n, red, green, blue); // pseudocode example
+  //int randomBrightness = random(100);
 
-  for (int i = 0; i < LED_COUNT; i++){
+  for (int i = 0; i < LED_COUNT; i++){ // iterates through the LED strip to give each one a unique color
     pixelSetRGB(i, colorControl);
   }
+  
+  strip.show(); // actually displays the what's set in PixelSetRGB / setPixelColor();
 
-//  for (int i = 0; i < LED_COUNT; i++){
-//    if (i % 5 == 0){
-//      pixelSet(i, orangeRGB);
-//    } else if (i % 8 == 0){
-//      pixelSet(i, medPinkRGB);
-//    } else {
-//      pixelSet(i, stormyRGB);
-//    }
-//  }
-  //strip.setBrightness(150 + randomBrightness);
-  strip.show();
-
-  Serial.println(colorControl);
+  //Serial.println(colorControl);
 
   colorControl++;
   colorControl %= 255;
-  delay(500);
+  if ((colorControl == 0) && (colorDirectionSwitch == false)){
+    colorDirectionSwitch = true;
+    Serial.println("True");
+  } else if ((colorControl == 0) && (colorDirectionSwitch == true)){
+    colorDirectionSwitch = false;
+    Serial.println("False");
+  }
+  
+  delay(50); // set this up to be controllable by a MAX patch
 }
 
-void pixelSet(int pixel, int color){
+void pixelSet(int pixel, int color){ // deprecated function as of 5/16/23
   strip.setPixelColor(pixel, color);
 }
 
-void pixelSetRGB(int pixel, int control){ //, int r, int g, int b){
-  float r = fmod(1 + pixel /*+ 2.8 * pixel*/ + control, 255);
-  float g = fmod(120 + pixel/*+ 2.8 * pixel*/ + control, 255);
-  float b = fmod(240 + pixel /*2.8 * pixel*/ + control, 255);
-//  float r = fmod(1  +  pixel * 2 + control, 255);
-//  float g = fmod(220 + pixel * 2 + control, 255);
-//  float b = fmod(250 + pixel * 2 + control, 255);
-//  float r = (pixel + control) % 255;
-//  float g = (120 + pixel + control) % 255;
-//  float b = (240 + pixel + control) % 255;
-  if (pixel == 5){
-    Serial.print("red: ");
-    Serial.println(r);
-    Serial.print("green: ");
-    Serial.println(g);
-    Serial.print("blue: ");
-    Serial.println(b);
+void pixelSetRGB(int pixel, int control){
+  //uint16_t hue = strip.Color(control + pixel, control - pixel, control);
+  uint16_t hue;
+  if (colorDirectionSwitch){
+    hue = fmod((control - pixel), 255);
+    hue = map(hue, 0, 255, 27000, 60000); // have this be a variable to be set by the MAX patch
+  } else {
+    hue = fmod((control - pixel), 255);
+    hue = map(hue, 0, 255, 60000, 27000);
   }
-  strip.setPixelColor(pixel, r, g, b);
+  // BUILD A BOOLEAN STATEMENT TO HAVE THINGS RAMP BACK AND FORTH
+  uint32_t newColor = strip.ColorHSV(hue, 240, 240);
+  
+  if (pixel == 5){
+    Serial.print(control); Serial.print(", "); Serial.print(hue); Serial.print(", "); Serial.println(newColor);
+  }
+  strip.setPixelColor(pixel, newColor); // sets pixel color individually
 }
