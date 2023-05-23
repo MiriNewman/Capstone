509097da244a5d457b89ef2c6f5b922d4d686ef9
Miri Newman <fishernewman@gmail.com>
2023-05-23 11:41:34 -0700

    Ethernet works now! Yippee

--- LED_Light_for_Capstone_QNEthernet/LED_Light_for_Capstone_QNEthernet.ino
+++ LED_Light_for_Capstone_QNEthernet/LED_Light_for_Capstone_QNEthernet.ino
@@ -1,40 +1,25 @@
-//#include <NativeEthernet.h>
-//#include <SPI.h>
-#include <OSCMessage.h>
+#include <LiteOSCParser.h>
+
 #include <Adafruit_NeoPixel.h>
 #include <math.h>
 #include <QNEthernet.h>
-#include <TimeLib.h>
-#include <LibPrintf.h>
+
+#include "OSC.h"
+//#include <LibPrintf.h>
 
 using namespace qindesign::network;
 
 // IP scheme 10.0.0.x
 // IP scheme 10.0.10.x for ESP (rainstick, etc)
 
-constexpr uint32_t kDHCPTimeout = 15000;
-constexpr uint16_t kNTPPort = 123;
+// TEST TEENSY IP: 192.168.1.100
 
-constexpr uint32_t kEpochDiff = 2'208'988'800;
-constexpr uint32_t kBreakTime =2'085'978'496;
+constexpr uint32_t kDHCPTimeout = 15000;  // 15 seconds
+constexpr uint16_t kOSCPort = 8000;
+constexpr char kServiceName[] = "osc-example";
 
 EthernetUDP udp;
 uint8_t buf[48];
-
-//byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-//char server[] = "www.arduino.cc";
-//IPAddress ip(10, 0, 0, 1); // number these sequentially for each cloud controller, 
-////REMEMEBER TO CHANGE IT!!!!
-//IPAddress myDNS(10, 10, 0, 1); // ??
-//
-//EthernetClient client;
-//
-//unsigned long beginMicros, endMicros;
-//unsigned long byteCount = 0;
-//unsigned long lastConnectionTime = 0;
-//const unsigned long postingInterval = 10*1000;
-//bool printWebData = true; // something to do with speed measurement?
-
 
 bool ethernetIsConnected = false;
 
@@ -57,102 +42,20 @@ bool colorDirectionSwitch = false;
 
 void setup() {
   Serial.begin(9600);
-  //Ethernet.begin();
-
-//  Ethernet.init(ETHERNET_PIN);
-//  
+  NetworkBegin();
+  
   strip.begin();
   strip.show(); // these reset the LED memory to start afresh
 
-//  Serial.println("Initialize Ethernet with DHCP:");
-//  if (Ethernet.begin(mac) == 0){
-//    Serial.println("Failed to configure Ethernet using DHCP");
-//    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-//      Serial.println("Ethernet shield was not found. Check physical connection or replace Teensy. <3");
-//      while (true) {
-//      }
-//    }
-//    if (Ethernet.linkStatus() == LinkOFF) {
-//      Serial.println("Ethernet cable is not connected.");
-//    }
-//    Ethernet.begin(mac, ip, myDNS);
-//    Serial.print("My IP address: ");
-//    Serial.println(Ethernet.localIP());
-//  } else {
-//    Serial.print(" DHCP assigned IP ");
-//    Serial.println(Ethernet.localIP());
-//  }
-//  delay(1000); // giving ethernet shield a minut to initialize
-//  Serial.print ("Connecting to "); Serial.print(server); Serial.println(". . .");
-//
-//  if (client.connect(server, 80)) {
-//    Serial.print("Connected to "); Serial.println(client.remoteIP());
-//    client.println("GET /search?q=arduino HTTP/1.1");
-//    client.println("Host: www.google.com");
-//    client.println("Connection: close");
-//    client.println();
-//  } else {
-//    Serial.println("Connection failed. :-(");
-//  }
-//  beginMicros = micros();
-
-  while (!Serial && millis() < 4000) {
-    // waiting for monitor
-  }
-  printf("Starting...\r\n");
-
-  uint8_t mac[6];
-  Ethernet.macAddress(mac);
-  printf("MAC = %02x:%02x:%02x:%02x:%02x:%02x\r\n",
-         mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-  printf("Starting Ethernet with DCHP...\r\n");
-  Ethernet.onLinkState([](bool state){
-    printf("[Ethernet] Link %s\n", state ? "ON" : "OFF");
-  });
-  if(!Ethernet.begin()) {
-    printf("Failed to start Ethernet\r\n");
-    return;
-  }
-  if (!Ethernet.waitForLocalIP(kDHCPTimeout)) {
-    printf("Failed to get IP address from DHCP\r\n");
-    return;
-  }
-  //IPAddress ip = (10,0,0,1);
-  IPAddress ip = Ethernet.localIP();
-  printf("    Local IP    = %u.%u.%u.%u\r\n", ip[0], ip[1], ip[2], ip[3]);
-  ip = Ethernet.subnetMask();
-  printf("    Subnet mask = %u.%u.%u.%u\r\n", ip[0], ip[1], ip[2], ip[3]);
-  ip = Ethernet.gatewayIP();
-  printf("    Gateway     = %u.%u.%u.%u\r\n", ip[0], ip[1], ip[2], ip[3]);
-  ip = Ethernet.dnsServerIP();
-  printf("    DNS         = %u.%u.%u.%u\r\n", ip[0], ip[1], ip[2], ip[3]);
-
-  udp.begin(kNTPPort);
-
-  memset(buf, 0, 48);
-  buf[0] = 0b00'100'011; // LI = 0, VN = 4, Mode = 3 (Client) // ??
-  uint32_t t = Teensy3Clock.get();
-  if (t <= kBreakTime) {
-    t -= kBreakTime;
-  } else {
-    t += kEpochDiff;
-  }
-
-  buf[40] = t >> 24;
-  buf[41] = t >> 16;
-  buf[42] = t >> 8;
-  buf[43] = t;
-
-  printf("Sending SNTP request to the gateway. . .");
-  if (udp.send(Ethernet.gatewayIP(), kNTPPort, buf, 48)) {
-    printf("ERROR");
-  }
-  printf("\r\n");
-
-  Serial.println("If you read this before anything else, someone is bad at writing libraries.");
 }
 
 void loop() {
+  int size = udp.parsePacket();
+  if (0 < size && static_cast<unsigned int>(size) <= sizeof(buf)) {
+    udp.read(buf, size);
+    printOSC(Serial, buf, size);
+  }
+  
   for (int i = 0; i < LED_COUNT; i++){ // iterates through the LED strip to give each one a unique color
     pixelSetRGB(i, colorControl);
   }
@@ -193,4 +96,58 @@ void pixelSetRGB(int pixel, int control){
 //    Serial.print(control); Serial.print(", "); Serial.print(hue); Serial.print(", "); Serial.println(newColor);
 //  }
   strip.setPixelColor(pixel, newColor); // sets pixel color individually
+}
+
+void NetworkBegin(){
+  while (!Serial && millis() < 4000) {
+    // Wait for Serial
+  }
+  Serial.println("Starting...");
+
+  // Print the MAC address
+  uint8_t mac[6];
+  Ethernet.macAddress(mac);  // This is informative; it retrieves, not sets
+  Serial.printf("MAC = %02x:%02x:%02x:%02x:%02x:%02x\r\n",
+                mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+
+  // Initialize Ethernet, in this case with DHCP
+  Serial.println("Starting Ethernet with DHCP...");
+  if (!Ethernet.begin()) {
+    Serial.println("Failed to start Ethernet");
+    return;
+  }
+  if (!Ethernet.waitForLocalIP(kDHCPTimeout)) {
+    Serial.println("Failed to get IP address from DHCP");
+    return;
+  }
+
+  IPAddress ip = Ethernet.localIP();
+  Serial.printf("    Local IP     = %u.%u.%u.%u\r\n", ip[0], ip[1], ip[2], ip[3]);
+  ip = Ethernet.subnetMask();
+  Serial.printf("    Subnet mask  = %u.%u.%u.%u\r\n", ip[0], ip[1], ip[2], ip[3]);
+  ip = Ethernet.broadcastIP();
+  Serial.printf("    Broadcast IP = %u.%u.%u.%u\r\n", ip[0], ip[1], ip[2], ip[3]);
+  ip = Ethernet.gatewayIP();
+  Serial.printf("    Gateway      = %u.%u.%u.%u\r\n", ip[0], ip[1], ip[2], ip[3]);
+  ip = Ethernet.dnsServerIP();
+  Serial.printf("    DNS          = %u.%u.%u.%u\r\n", ip[0], ip[1], ip[2], ip[3]);
+
+  // Listen on port and start an mDNS service
+  udp.begin(kOSCPort);
+  Serial.println("Starting mDNS...");
+  if (!MDNS.begin(kServiceName)) {
+    Serial.println("ERROR: Starting mDNS.");
+  } else {
+    if (!MDNS.addService("_osc", "_udp", kOSCPort)) {
+      Serial.println("ERROR: Adding service.");
+    } else {
+      Serial.printf("Started mDNS service:\r\n"
+                    "    Name: %s\r\n"
+                    "    Type: _osc._udp\r\n"
+                    "    Port: %u\r\n",
+                    kServiceName, kOSCPort);
+    }
+  }
+
+  Serial.println("Waiting for OSC messages...");
 }
